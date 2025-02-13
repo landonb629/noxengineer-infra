c8f407a71edfcfef580d7982be9fc8ff633cd7e7
landon babay <landon.babay@caylent.com>
2025-02-12 22:50:44 -0500

    starting the code for the terraform backend

---
+++ main.tf
@@ -0,0 +1,9 @@
+module "s3-bucket" {
+  source  = "terraform-aws-modules/s3-bucket/aws"
+  version = "4.6.0"
+
+  bucket = "noxengineer-deployment-prod"
+  website = { 
+    
+  }
+}
---
+++ provider.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+  backend "s3" { 
+    bucket = "noxengineer-terraform-backend"
+    key = "prod/terraform.tfstate"
+    region = "us-east-1"
+    profile = "lbabay"
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+  profile = "lbabay"
+}

0210eb34cb7ca09c6177a5a4e35365de91a7e432
landon babay <landon.babay@caylent.com>
2025-02-15 07:55:40 -0500

    adding fix for the routing errors on cloudfront for react router

--- main.tf
+++ main.tf
@@ -59,16 +59,28 @@ module "cloudfront_distro" {
       origin_access_control = "s3_oac"
     }
   }
-  viewer_certificate = { 
+  viewer_certificate = {
     acm_certificate_arn = module.acm.acm_certificate_arn
     ssl_support_method = "sni-only"
   }
-  default_cache_behavior = { 
+  default_cache_behavior = {
     target_origin_id = "noxengineer"
     viewer_protocol_policy = "https-only"
     allowed_methods = ["GET", "HEAD", "OPTIONS"]
     cached_methods = ["GET", "HEAD"]
   }
+  custom_error_response = [
+    {
+      error_code = 404
+      response_code = 200
+      repsonse_page_path = "/index.html"
+    },
+    {
+      error_code = 403
+      response_code = 200
+      response_page_path = "/index.html"
+    }
+  ]
   depends_on = [ module.acm ]
 }
 
@@ -98,7 +110,7 @@ resource "aws_route53_record" "nox" {
   zone_id = "Z02327713O62WJSZVOOKF"
   name = "noxengineer.com"
   type = "A"
-   alias { 
+   alias {
     name = module.cloudfront_distro.cloudfront_distribution_domain_name
     zone_id = module.cloudfront_distro.cloudfront_distribution_hosted_zone_id
     evaluate_target_health = false
